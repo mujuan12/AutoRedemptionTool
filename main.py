28b8548b16da89bf97714fa958d55ad316dd85b4
liuyanze <1471059003@qq.com>
2023-07-31 21:54:01 +0800

    更新字符集

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import requests
 from bs4 import BeautifulSoup
 from urllib.parse import quote
 import json
+import os
+
+os.system('chcp 65001')
 
 giftCodeUrl = "http://statistics.pandadastudio.com/player/giftCodeView"
 infoUrl = r"E:\Microsoft Edge Downloads\uid.url"
