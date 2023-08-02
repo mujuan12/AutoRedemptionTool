314b1d9520f7d940527ca72620972a458eb7dc21
liuyanze <1471059003@qq.com>
2023-08-02 21:36:14 +0800

    优化兑换逻辑

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import requests
 from bs4 import BeautifulSoup
-import sys
+import time
 from urllib.parse import quote
 import json
+
 # -*- coding: utf-8 -*-
 
 
@@ -26,8 +27,8 @@ def simulate_submit_event(uid, dhm):
         uid) != 12 else "http://statistics_1.pandadastudio.com/player/giftCode"
 
     # 将中文转换为 URL 编码
-    #url_encoded_text = quote(dhm)
-    #url_encoded_text = dhm
+    # url_encoded_text = quote(dhm)
+    # url_encoded_text = dhm
 
     payload = {
         'uid': uid,
@@ -38,6 +39,7 @@ def simulate_submit_event(uid, dhm):
     response = requests.post(newurl, data=payload)
     if response is None:
         print("空")
+    print(response.text)
     return response.json()
 
 
@@ -49,44 +51,49 @@ def Open(filename):
             line = line.strip()
             if ' ' in line:
                 elements = line.split(' ')
-                if len(elements) == 2:
-                    data_list.append((elements[0], elements[1]))
+                if len(elements) == 3:
+                    data_list.append((elements[0], elements[1], elements[2]))
             else:
                 data_list.append(line)
+    file.close()
     return data_list
 
 
 def reward():
     uids = Open('uid.txt')
-    dhms = Open('dhm.txt')
+    dhms_LongTime = Open('dhm_LongTime')
+    dhms_limited = Open('dhm_limited')
 
     data = {}
     msg = ""
 
     for uid in uids:
-        flag = 0
         print(f"昵称：{uid[0]}")
         status = []
         tmp = "### {}\n\n".format(uid[0])
-        for dhm in dhms:
-            if dhm == "长期:" or dhm == "限时:":
-                if dhm == "限时:":
-                    flag = 1
-                continue
+        for dhm in dhms_LongTime:
+            if uid[2] == 'true':
+                break
             res = simulate_submit_event(uid=uid[1], dhm=dhm)
+            time.sleep(0.3)
 
-            if flag:
-                status.append(f'兑换码:{dhm}\n\n\t状态:{res}')
-            else:
-                if res['code'] != 425:
-                    status.append(f'兑换码:{dhm}\n\n\t状态:{res}')
+            if res['code'] != 425:
+                status.append(f'兑换码:{dhm}\n\n\t\t状态:{res["msg"]}')
+            print(f'\t·兑换码:{dhm}，状态:{res}')
+        for dhm in dhms_limited:
+            res = simulate_submit_event(uid=uid[1], dhm=dhm)
+            time.sleep(0.3)
+
+            status.append(f'兑换码:{dhm}\n\n\t\t状态:{res["msg"]}')
             print(f'\t·兑换码:{dhm}，状态:{res}')
         for stat in status:
             tmp = tmp + "\t{}\n\n".format(stat)
             msg = msg + tmp
-        #data[uid[0]] = tmp
-    #print(data)
+
+        modifyUidIsRewardLongDhm(uid=uid[1])
+
     return msg
+
 
 def sendMsg(data):
     notifyToken = "SCT218479TQMHt4Eig5ZIuhFRk8h8jOOyj"
@@ -98,6 +105,31 @@ def sendMsg(data):
     }
     requests.post(url.format(notifyToken), data=body)
     print("消息已通过 Serverchan-Turbo 推送，请检查推送结果")
+
+
+def modifyUidIsRewardLongDhm(uid):
+    with open('uid.txt', 'r+', encoding='utf-8') as file:
+        # 读取文件的每一行
+        lines = file.readlines()
+
+        # 遍历每一行
+        for i, line in enumerate(lines):
+            # 判断是否是指定的行
+            if uid in line:
+                # 在该行的末尾追加数据
+                index = line.find(' ', line.find(' ') + 1)
+                new_line = line[:index]
+                new_line += ' true\n'
+                # 将修改后的行写回文件
+                lines[i] = new_line
+                break
+
+        # 将修改后的内容写回文件
+        file.seek(0)
+        file.writelines(lines)
+
+        # 关闭文件
+        file.close()
 
 
 data = reward()
